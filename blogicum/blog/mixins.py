f6efcd846c8b541a41de5bb5bb11a417954e21e4
настя бб <133085628+evnlly@users.noreply.github.com>
2025-04-07 18:58:23 +0700

    Delete blogicum directory

--- blogicum/blog/mixins.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
-
-
-class AuthorOnlyMixin(UserPassesTestMixin):
-    def test_func(self):
-        return self.get_object().author == self.request.user
